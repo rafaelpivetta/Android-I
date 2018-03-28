02a5c4c7d489f9f09ad5f92f3fcd5f3be0dd57d2
Rafael Pivetta Balbuena <rafael.pivetta@gmail.com>
2018-03-28 13:32:34 -0300

    Atribuição de nomes aos textviews da calculadora.

--- Calculadora/app/src/main/java/br/iesb/mobile/calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/br/iesb/mobile/calculadora/MainActivity.kt
@@ -2,14 +2,31 @@ package br.iesb.mobile.calculadora
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        updateDisplay("")
     }
 
+    val listaOperacao: MutableList<String> = arrayListOf()
+    val numeroCache: MutableList<String> = arrayListOf()
+
+    fun limpar() {
+        numeroCache.clear()
+        listaOperacao.clear()
+    }
+
+    fun clickNumero(numero: String) {
+
+
+        numeroCache.add(numero)
+        
+    }
 
 
 }
