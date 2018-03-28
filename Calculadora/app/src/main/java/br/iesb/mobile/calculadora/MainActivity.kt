3ba8655dff7f825d506ba5d848799873c5079791
Rafael Pivetta Balbuena <rafael.pivetta@gmail.com>
2018-03-28 13:34:31 -0300

    Atualizacao class MainActivity

--- Calculadora/app/src/main/java/br/iesb/mobile/calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/br/iesb/mobile/calculadora/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        updateDisplay("")
+//        updateDisplay("")
     }
 
     val listaOperacao: MutableList<String> = arrayListOf()
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
 
 
         numeroCache.add(numero)
-        
+
     }
 
 
